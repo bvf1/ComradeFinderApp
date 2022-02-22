22b93fc42b7b82b03970b90a9ce30b65b50c7276
bvf1 <bvf1@hi.is>
2022-02-22 12:19:38 +0000

    -a

--- app/src/main/java/is/hbv2/ComradeFinderApp/MakeApplicationActivity.java
+++ app/src/main/java/is/hbv2/ComradeFinderApp/MakeApplicationActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MakeApplicationActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
