d97064f9928cbd8f362941dd8cdf11b2ca62c2cf
bvf1 <bvf1@hi.is>
2022-02-22 01:05:39 +0000

    Advertisement & Application view komin

---
+++ app/src/main/java/is/hbv2/ComradeFinderApp/MakeAdvertisementActivity.java
@@ -0,0 +1,14 @@
+package is.hbv2.ComradeFinderApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MakeAdvertisementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_advertisement);
+    }
+}
