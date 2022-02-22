977accc2e17367687b804c69c8d19ef3a69e959a
bvf1 <bvf1@hi.is>
2022-02-22 16:05:05 +0000

    started the make Activities, needs backend access. minor fix on the make layouts

--- app/src/main/java/is/hbv2/ComradeFinderApp/MakeAdvertisementActivity.java
+++ app/src/main/java/is/hbv2/ComradeFinderApp/MakeAdvertisementActivity.java
@@ -3,12 +3,45 @@ package is.hbv2.ComradeFinderApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.slider.RangeSlider;
 
 public class MakeAdvertisementActivity extends AppCompatActivity {
+
+    private Button mAdButton;
+
+    private EditText mTitle;
+    private RangeSlider mSalary ; // what kind of widget to use
+    private EditText mDescription;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_advertisement);
+
+        mAdButton = findViewById(R.id.registerAd_button);
+        mAdButton.setOnClickListener(view -> {
+
+            mTitle = findViewById(R.id.title_entry);
+            mSalary = findViewById(R.id.priceRange_entry);
+            mDescription = findViewById(R.id.description_entry);
+
+            getInfoFromForm();
+            /// where do we want to go
+        });
+    }
+
+
+
+    private void getInfoFromForm() {
+        String title = mTitle.getText().toString();
+        // mSalary
+        String description = mDescription.getText().toString();
+
+        // put into backend
     }
 }
--- app/src/main/java/is/hbv2/ComradeFinderApp/MakeApplicationActivity.java
+++ app/src/main/java/is/hbv2/ComradeFinderApp/MakeApplicationActivity.java
@@ -3,13 +3,53 @@ package is.hbv2.ComradeFinderApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MakeApplicationActivity extends AppCompatActivity {
+
+    private Button mAppButton;
+
+    private EditText mName;
+    private EditText mEmail;
+    private EditText mPhoneNumber;
+    private EditText mPosition;
+    private EditText mLastCompany;
+    private EditText mExtra;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_application);
+
+        mAppButton = findViewById(R.id.registerApp_button);
+        mAppButton.setOnClickListener(view -> {
+            mName = findViewById(R.id.name_Entry);
+            mEmail = findViewById(R.id.email_Entry);
+            mPhoneNumber = findViewById(R.id.phone_Entry);
+            mPosition = findViewById(R.id.position_Entry);
+            mLastCompany = findViewById(R.id.lastCompany_Entry);
+            mExtra = findViewById(R.id.adExtra_Entry);
+
+
+            getInfoFromForm();
+            /// where do we want to got
+            // put info in backend
+        });
+    }
+
+
+
+    private void getInfoFromForm() {
+        String name = mName.getText().toString();
+        String email = mEmail.getText().toString();
+        String phoneNumber = mPhoneNumber.getText().toString();
+        String position = mPosition.getText().toString();
+        String lastCompany = mLastCompany.getText().toString();
+        String extra = mExtra.getText().toString();
+
+        // put info into database
+
     }
 }
