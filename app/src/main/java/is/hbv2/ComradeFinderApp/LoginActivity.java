fe6d5424d104935c93b1111097e72f5c2e84763b
gvg8 <gvg8@hi.is>
2022-02-22 10:38:24 +0000

    Login activity looks good. No functionality as is. Dumped out code from old preset

---
+++ app/src/main/java/is/hbv2/ComradeFinderApp/LoginActivity.java
@@ -0,0 +1,29 @@
+package is.hbv2.ComradeFinderApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    /*
+        mLoginButton = (Button) findViewById(R.id.login_button);
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+        /**/
+    }
+}
