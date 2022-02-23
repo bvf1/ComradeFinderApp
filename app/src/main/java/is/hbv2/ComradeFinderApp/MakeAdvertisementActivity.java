6c4f991a2d2e4c53fac1b01ce941c768d107b3ea
bvf1 <bvf1@hi.is>
2022-02-23 20:34:17 +0000

    added to makeadverticement layout+view

--- app/src/main/java/is/hbv2/ComradeFinderApp/MakeAdvertisementActivity.java
+++ app/src/main/java/is/hbv2/ComradeFinderApp/MakeAdvertisementActivity.java
@@ -3,20 +3,31 @@ package is.hbv2.ComradeFinderApp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.google.android.material.slider.RangeSlider;
 
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
 public class MakeAdvertisementActivity extends AppCompatActivity {
 
+
+
     private Button mAdButton;
+    private Button mAddQuestionButton;
 
     private EditText mTitle;
     private RangeSlider mSalary ; // what kind of widget to use
     private EditText mDescription;
-
+    private ArrayList<String> questions = new ArrayList<>();
+    private ListView mQuestionsView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +36,6 @@ public class MakeAdvertisementActivity extends AppCompatActivity {
 
         mAdButton = findViewById(R.id.registerAd_button);
         mAdButton.setOnClickListener(view -> {
-
             mTitle = findViewById(R.id.title_entry);
             mSalary = findViewById(R.id.priceRange_entry);
             mDescription = findViewById(R.id.description_entry);
@@ -33,9 +43,34 @@ public class MakeAdvertisementActivity extends AppCompatActivity {
             getInfoFromForm();
             /// where do we want to go
         });
+        mAddQuestionButton = (Button) findViewById(R.id.addQuestion_button);
+        mAddQuestionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText text = findViewById(R.id.addQuestion);
+                String question = text.getText().toString();
+                mQuestionsView = findViewById(R.id.extraQuestionsView);
+
+                if(questions.contains(question)) {
+                    Toast.makeText(getBaseContext(), "Item Already Added to the Array", Toast.LENGTH_LONG).show();
+                }
+
+                else if (question == null || question.trim().equals("")) {
+                    Toast.makeText(getBaseContext(), "Input Fields is Empty", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    questions.add(question);
+                   // ArrayAdapter<String> adapter = new ArrayAdapter<String>(MakeAdvertisementActivity.this, android.R.layout.simple_list_item_1, questions);
+                  //  mQuestionsView.setAdapter(adapter);
+                    ((EditText) findViewById(R.id.addQuestion)).setText("");
+                }
+
+
+            }
+        });
+
+
     }
-
-
 
     private void getInfoFromForm() {
         String title = mTitle.getText().toString();
@@ -43,5 +78,10 @@ public class MakeAdvertisementActivity extends AppCompatActivity {
         String description = mDescription.getText().toString();
 
         // put into backend
+    }
+
+
+    private void printlist() {
+        
     }
 }
