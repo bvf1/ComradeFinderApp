4657c8ad3a1253fabb3fcee47744eac161c41eb8
gvg8 <gvg8@hi.is>
2022-03-08 13:04:19 +0000

    Registry activity looks good. Company option added.

--- app/src/main/java/is/hbv2/ComradeFinderApp/RegisterActivity.java
+++ app/src/main/java/is/hbv2/ComradeFinderApp/RegisterActivity.java
@@ -5,13 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.Switch;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText mUsername;
     private EditText mPassword;
+    private EditText mPhone;
+    private EditText mEmail;
     private Button mRegisterBtn;
+    private Switch mCompanySwitch;
+    private EditText mCompanyName;
+    private EditText mSSN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,8 @@ public class RegisterActivity extends AppCompatActivity {
 
         mUsername = (EditText) findViewById(R.id.editTextRegisterUsername);
         mPassword = (EditText) findViewById(R.id.editTextPassword);
+        mPhone = (EditText) findViewById(R.id.editTextPhone);
+        mEmail = (EditText) findViewById(R.id.editTextEmailAddress);
 
         mRegisterBtn = (Button) findViewById(R.id.register_button);
         mRegisterBtn.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +36,23 @@ public class RegisterActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String username = mUsername.getText().toString();
                 // TODO: Handle registration
+            }
+        });
+
+        mCompanyName = (EditText) findViewById(R.id.editTextCompanyName);
+        mSSN = (EditText) findViewById(R.id.editTextSSN);
+
+        mCompanySwitch = (Switch) findViewById(R.id.isCompanySwitch);
+        mCompanySwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b) {
+                    mCompanyName.setVisibility(View.VISIBLE);
+                    mSSN.setVisibility(View.VISIBLE);
+                } else {
+                    mCompanyName.setVisibility(View.GONE);
+                    mSSN.setVisibility(View.GONE);
+                }
             }
         });
 
