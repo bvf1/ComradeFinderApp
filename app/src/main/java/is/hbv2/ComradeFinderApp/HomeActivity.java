877964d175f1562f9cb70bc2ccbe9ce6c0d2e1c6
JonAlex99 <jonalexpetursson@gmail.com>
2022-03-09 19:21:00 +0000

    Bæti við java file fyrir home

---
+++ app/src/main/java/is/hbv2/ComradeFinderApp/HomeActivity.java
@@ -0,0 +1,17 @@
+package is.hbv2.ComradeFinderApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        setContentView(R.layout.activity_home);
+
+    }
+}
