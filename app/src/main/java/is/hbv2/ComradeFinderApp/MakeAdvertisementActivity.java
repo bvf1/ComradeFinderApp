5e456dd283b326d9292f0891c72f8fc73dabb3b3
bvf1 <bvf1@hi.is>
2022-03-10 23:35:03 +0000

    makeAdvertisement listview can be added to

--- app/src/main/java/is/hbv2/ComradeFinderApp/MakeAdvertisementActivity.java
+++ app/src/main/java/is/hbv2/ComradeFinderApp/MakeAdvertisementActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -29,11 +30,65 @@ public class MakeAdvertisementActivity extends AppCompatActivity {
     private ArrayList<String> questions = new ArrayList<>();
     private ListView mQuestionsView;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_advertisement);
+      //  makeList();
 
+        // adding questions to list view
+        mQuestionsView = findViewById(R.id.extraQuestionsView);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                MakeAdvertisementActivity.
+                        this, android.R.layout.simple_list_item_1, questions);
+        mQuestionsView.setAdapter(arrayAdapter);
+
+        // adding item to questions
+        mAddQuestionButton = (Button) findViewById(R.id.addQuestion_button);
+        mAddQuestionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                EditText text = findViewById(R.id.addQuestion);
+                String question = text.getText().toString();
+
+                if(questions.contains(question)) {
+                    Toast.makeText(getBaseContext(), "Item Already Added to the Array", Toast.LENGTH_LONG).show();
+                }
+
+                else if (question == null || question.trim().equals("")) {
+                    Toast.makeText(getBaseContext(), "Input Fields is Empty", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    questions.add(question);
+                    mQuestionsView.setAdapter(arrayAdapter);
+                    ((EditText) findViewById(R.id.addQuestion)).setText("");
+                }
+            }
+        });
+
+        // changing items
+        mQuestionsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedQuestion = questions.get(posti)
+            }
+        });
+        /*
+        mQuestionsView.setOnItemClickListener(new OnItemClickListener()
+        {
+            // argument position gives the index of item which is clicked
+            public void onItemClick(AdapterView<?> arg0, View v,int position, long arg3)
+            {
+                String selectedmovie=movies.get(position);
+                Toast.makeText(getApplicationContext(), "Movie Selected : "+selectedmovie,   Toast.LENGTH_LONG).show();
+            }
+        });*/
+
+
+
+        // register ad
         mAdButton = findViewById(R.id.registerAd_button);
         mAdButton.setOnClickListener(view -> {
             mTitle = findViewById(R.id.title_entry);
@@ -42,7 +97,7 @@ public class MakeAdvertisementActivity extends AppCompatActivity {
 
             getInfoFromForm();
             /// where do we want to go
-        });
+        });/*
         mAddQuestionButton = (Button) findViewById(R.id.addQuestion_button);
         mAddQuestionButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,7 +105,7 @@ public class MakeAdvertisementActivity extends AppCompatActivity {
                 EditText text = findViewById(R.id.addQuestion);
                 String question = text.getText().toString();
                 mQuestionsView = findViewById(R.id.extraQuestionsView);
-
+/*--
                 if(questions.contains(question)) {
                     Toast.makeText(getBaseContext(), "Item Already Added to the Array", Toast.LENGTH_LONG).show();
                 }
@@ -66,8 +121,8 @@ public class MakeAdvertisementActivity extends AppCompatActivity {
                 }
 
 
-            }
-        });
+
+        });*/
 
 
     }
@@ -81,7 +136,12 @@ public class MakeAdvertisementActivity extends AppCompatActivity {
     }
 
 
+    private void makeList()  {
+        questions.add("question 1");
+        questions.add("question 2");
+
+    }
     private void printlist() {
-        
+
     }
 }
