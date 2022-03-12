8c3e4e511a97929347c7ab69791d4584be7ee683
bvf1 <bvf1@hi.is>
2022-03-12 19:31:49 +0000

    added tags to makeAdvertisement

--- app/src/main/java/is/hbv2/ComradeFinderApp/MakeAdvertisementActivity.java
+++ app/src/main/java/is/hbv2/ComradeFinderApp/MakeAdvertisementActivity.java
@@ -1,26 +1,23 @@
 package is.hbv2.ComradeFinderApp;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
+import android.widget.Adapter;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.google.android.material.slider.LabelFormatter;
 import com.google.android.material.slider.RangeSlider;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
-import java.util.logging.Logger;
 
 public class MakeAdvertisementActivity extends AppCompatActivity {
 
@@ -28,70 +25,95 @@ public class MakeAdvertisementActivity extends AppCompatActivity {
 
     private Button mAdButton;
     private Button mAddQuestionButton;
+    private Button mAddTagsButton;
+
 
     private EditText mTitle;
     private RangeSlider mSalary ; // what kind of widget to use
     private EditText mDescription;
     private ArrayList<String> questions = new ArrayList<>();
+    private ArrayList<String> addedTags = new ArrayList<>();
     private ListView mQuestionsView;
+    private ListView mAddedTagsView;
+    private Spinner mTags;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_advertisement);
-      //  makeList();
+
+
 
         // adding questions to list view
         mQuestionsView = findViewById(R.id.extraQuestionsView);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+        ArrayAdapter<String> questionAdapter = new ArrayAdapter<String>(
                 MakeAdvertisementActivity.
                         this, android.R.layout.simple_list_item_1, questions);
-        mQuestionsView.setAdapter(arrayAdapter);
+        mQuestionsView.setAdapter(questionAdapter);
 
         // adding item to questions
         mAddQuestionButton = (Button) findViewById(R.id.addQuestion_button);
-        mAddQuestionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        mAddQuestionButton.setOnClickListener(view -> {
+            EditText text = findViewById(R.id.addQuestion);
+            String question = text.getText().toString();
+            if(questions.contains(question)) {
+                Toast.makeText(getBaseContext(), "Item Already Added to the Array", Toast.LENGTH_LONG).show();
+            }
 
-                EditText text = findViewById(R.id.addQuestion);
-                String question = text.getText().toString();
-
-                if(questions.contains(question)) {
-                    Toast.makeText(getBaseContext(), "Item Already Added to the Array", Toast.LENGTH_LONG).show();
-                }
-
-                else if (question == null || question.trim().equals("")) {
-                    Toast.makeText(getBaseContext(), "Input Fields is Empty", Toast.LENGTH_LONG).show();
-                }
-                else {
-                    questions.add(question);
-                    mQuestionsView.setAdapter(arrayAdapter);
-                    ((EditText) findViewById(R.id.addQuestion)).setText("");
-                }
+            else if (question == null || question.trim().equals("")) {
+                Toast.makeText(getBaseContext(), "Input Fields is Empty", Toast.LENGTH_LONG).show();
+            }
+            else {
+                questions.add(question);
+                mQuestionsView.setAdapter(questionAdapter);
+                ((EditText) findViewById(R.id.addQuestion)).setText("");
             }
         });
 
         // changing items
-        mQuestionsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                questions.remove(i);
-                arrayAdapter.notifyDataSetChanged();
-            }
+        mQuestionsView.setOnItemClickListener((adapterView, view, i, l) -> {
+            questions.remove(i);
+            questionAdapter.notifyDataSetChanged();
         });
 
         // priceRange
-        RangeSlider priceSlider = findViewById(R.id.priceRange_entry);
-        priceSlider.setLabelFormatter(new LabelFormatter() {
-            @NonNull
-            @Override
-            public String getFormattedValue(float value) {
-                //It is just an example
-                if (value == 3.0f)
-                    return "TEST";
-                return String.format("%,.0f", value);
+        mSalary = findViewById(R.id.priceRange_entry);
+        mSalary.setLabelFormatter(value -> {
+            //It is just an example
+            if (value == 3.0f)
+                return "TEST";
+            return String.format("%,.0f", value);
+        });
+
+        // adding tags
+        String[] tags = { "tag 1", "tag 2", "tag 3"};
+        mTags = findViewById(R.id.allTags);
+        ArrayAdapter tagAdapter = new ArrayAdapter(this,android.R.layout.simple_spinner_item,tags);
+        tagAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //Setting the ArrayAdapter data on the Spinner
+        mTags.setAdapter(tagAdapter);
+
+        // adding tags to list view
+        mAddedTagsView = findViewById(R.id.addedTagsView);
+        ArrayAdapter<String> addedTagsAdapter = new ArrayAdapter<String>(
+                MakeAdvertisementActivity.this,
+                android.R.layout.simple_list_item_1, addedTags);
+        mAddedTagsView.setAdapter(addedTagsAdapter);
+
+
+        // add t
+        mAddTagsButton = (Button) findViewById(R.id.addTag_button);
+        mAddTagsButton.setOnClickListener(view -> {
+            int index = mTags.getLastVisiblePosition();
+            String tag = tags[index];
+
+            if(addedTags.contains(tag)) {
+                Toast.makeText(getBaseContext(), "Item Already Added to the Array", Toast.LENGTH_LONG).show();
+            }
+            else {
+                addedTags.add(tag);
+                mAddedTagsView.setAdapter(addedTagsAdapter);
             }
         });
 
@@ -115,7 +137,7 @@ public class MakeAdvertisementActivity extends AppCompatActivity {
     private void getInfoFromForm() {
         String title = mTitle.getText().toString();
         // mSalary
-        List salary = mSalary.getValues();
+        List<Float> salary = mSalary.getValues();
         //Log.i("salary", typeOf salary);
         String description = mDescription.getText().toString();
 
