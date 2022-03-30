44d2a9a851c69dc2088c018c10a505d389142e74
gvg8 <gvg8@hi.is>
2022-03-30 13:57:39 +0000

    Network manager in progress
    Added volley dependency

---
+++ app/src/main/java/is/hbv2/ComradeFinderApp/Network/NetworkManager.java
@@ -0,0 +1,44 @@
+package is.hbv2.ComradeFinderApp.Network;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class NetworkManager {
+
+    private static final String BASE_URL = "http://10.0.2.2:8080/";
+
+    private static NetworkManager mInstance;
+    private static RequestQueue mQueue;
+    private Context mContext;
+
+    public static synchronized NetworkManager getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new NetworkManager(context);
+        }
+        return mInstance;
+    }
+
+    private NetworkManager(Context context) {
+        mContext = context;
+        mQueue = getRequestQueue();
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mQueue == null) {
+            mQueue = Volley.newRequestQueue(mContext.getApplicationContext());
+        }
+        return mQueue;
+    }
+
+    /*
+    public void getUsers() {
+        StringRequest request = new StringRequest(
+                Request.Method.GET, BASE_URL +
+        );
+    }
+    /* == We might need more mappings on the backend to support what we need to do == */
+}
