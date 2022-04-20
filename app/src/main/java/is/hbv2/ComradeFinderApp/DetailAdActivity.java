925e4f70f07020a01b0e430b3bc251db99da5001
gvg8 <gvg8@hi.is>
2022-04-20 18:29:13 +0000

    Dummy Ads in DetailAdActivity

--- app/src/main/java/is/hbv2/ComradeFinderApp/DetailAdActivity.java
+++ app/src/main/java/is/hbv2/ComradeFinderApp/DetailAdActivity.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import is.hbv2.ComradeFinderApp.Entities.Ad;
 
 public class DetailAdActivity extends AppCompatActivity {
@@ -23,6 +26,9 @@ public class DetailAdActivity extends AppCompatActivity {
     private void getSlecetedAd() {
         Intent previousIntent = getIntent();
         Long parsedStringID = previousIntent.getLongExtra("id", 0);
+        if (HomeActivity.ads.size() == 0) {
+            HomeActivity.ads.addAll(dummyAds());
+        }
         selectedAd = HomeActivity.ads.get(Math.toIntExact((parsedStringID)));
     }
 
@@ -32,5 +38,22 @@ public class DetailAdActivity extends AppCompatActivity {
 
         adName.setText(selectedAd.getTitle());
         adSalary.setText(selectedAd.getSalaryRange());
+    }
+
+    private List<Ad> dummyAds() {
+        List<Ad> ads = new ArrayList<>();
+
+        Ad testAd1 = new Ad("Title1", "Description1", "1-1", new ArrayList<>(), "Comapny1", "LinkToImage1", new ArrayList<>());
+        Ad testAd2 = new Ad("Title2", "Description2", "2-2", new ArrayList<>(), "Comapny2", "LinkToImage2", new ArrayList<>());
+        Ad testAd3 = new Ad("Title3", "Description3", "3-3", new ArrayList<>(), "Comapny3", "LinkToImage3", new ArrayList<>());
+
+        testAd1.setID(0);
+        testAd2.setID(1);
+        testAd3.setID(2);
+
+        ads.add(testAd1);
+        ads.add(testAd2);
+        ads.add(testAd3);
+        return ads;
     }
 }
