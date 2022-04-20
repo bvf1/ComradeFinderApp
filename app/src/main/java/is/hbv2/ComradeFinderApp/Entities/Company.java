d17dddb41e25d6d5a1e53a0ffd0dc8c5019c4617
gvg8 <gvg8@hi.is>
2022-04-20 20:39:58 +0000

    Converting Company.SSN from int to String

--- app/src/main/java/is/hbv2/ComradeFinderApp/Entities/Company.java
+++ app/src/main/java/is/hbv2/ComradeFinderApp/Entities/Company.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class Company extends Account {
 
     @SerializedName("SSN")
-    private int SSN;
+    private String SSN;
     @SerializedName("street")
     private String street;
     @SerializedName("companyPhone")
@@ -21,7 +21,7 @@ public class Company extends Account {
 
     public Company(String username, String password, String phone,
                    String email, String displayName, String description,
-                   int SSN, String street, String companyPhone) {
+                   String SSN, String street, String companyPhone) {
         this.username = username;
         this.password = password;
         this.phone = phone;
@@ -35,11 +35,11 @@ public class Company extends Account {
 
     // GETTERS AND SETTERS
 
-    public int getSSN() {
+    public String getSSN() {
         return SSN;
     }
 
-    public void setSSN(int SSN) {
+    public void setSSN(String SSN) {
         this.SSN = SSN;
     }
 
